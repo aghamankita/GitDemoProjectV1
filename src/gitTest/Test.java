745d15a54a101303ce5deff88baf53fd91758c36
Ankita <ankitaagham94@gmail.com>
2022-06-10 10:31:41 +0530

    First Commit...

---
+++ src/gitTest/Test.java
@@ -0,0 +1,18 @@
+package gitTest;
+
+public class Test {
+
+	public static void main(String[] args) {
+	
+		 String hungry = "yes";
+		 	if(hungry=="yes") {
+		 		System.out.println("Eat Somthing.....");
+		 	}
+		 	
+		 	else {
+		 		System.out.println("Do work......");
+		 	}
+
+	}
+
+}
